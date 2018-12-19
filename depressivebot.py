d021b1ae416a43e5cf7fbe1ce059de5a86e7c4ec
amaurs <amaury.gtz@gmail.com>
2018-12-19 01:20:07 -0800

    🔌 Working with resource from S3.

--- depressivebot.py
+++ depressivebot.py
@@ -1,14 +1,15 @@
-from __future__ import unicode_literals
-
 import tweepy
 import markovify
 import random
 import os
+import boto3
 
-CONSUMER_KEY=os.environ.get("CONSUMER_KEY")
-CONSUMER_SECRET=os.environ.get("CONSUMER_SECRET")
-ACCESS_TOKEN=os.environ.get("ACCESS_TOKEN")
-ACCESS_TOKEN_SECRET=os.environ.get("ACCESS_TOKEN_SECRET")
+CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
+CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
+BUCKET = os.environ.get("BUCKET")
+KEY = os.environ.get("KEY")
 
 def tweet_something(status):
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -17,9 +18,14 @@ def tweet_something(status):
     api.update_status(status)
     api = None
 
+def get_corpus_from_s3():
+    s3 = boto3.resource('s3')
+    obj = s3.Object(BUCKET, KEY)
+    text = obj.get()['Body'].read().decode('utf-8')
+    return text
+
 def main():
-    with open("robot.txt") as f:
-        text = f.read()
+    text = get_corpus_from_s3()
     text_model = markovify.Text(text)
     tweet_something(text_model.make_sentence())
 
